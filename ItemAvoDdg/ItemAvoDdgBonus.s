fbdc03b80a2f5734548ee0eab76317ee3eb8e8d1
Vexalika <109774270+Vexalika@users.noreply.github.com>
2023-08-31 02:21:54 -0500

    Add files via upload

--- ItemAvoDdg/ItemAvoDdgBonus.s
+++ ItemAvoDdg/ItemAvoDdgBonus.s
@@ -19,7 +19,7 @@ ldrh r0,[r0]
 mov r2,#0xFF
 and r0,r2
 cmp r0,#0
-beq CheckDefender
+beq End
 
 
 ldr r3,=ItemAvoBonusList
@@ -83,14 +83,14 @@ ldrh r0,[r0]
 mov r2,#0xFF
 and r0,r2
 cmp r0,#0
-beq CheckDefender
+beq End
 
 ldr r3,=ItemDdgBonusList
 
 DdgLoopStart:
 ldrb r2,[r3]
 cmp r2,#0
-beq CheckDefender
+beq End
 
 cmp r2,r0
 beq NextAtkrDdg
@@ -117,7 +117,7 @@ add r3,#0x68  		@given stat
 ldrb r2,[r3]	
 add r2,r1		@add given value
 strb r2,[r3]	@store
-b CheckDefender
+b End
 
 
 AtkrDdgPenalty:
@@ -131,143 +131,11 @@ ble SetZeroDdgA
 
 sub r2,r1		@subtract given value
 strb r2,[r3]	@store
-b CheckDefender
+b End
 
 SetZeroDdgA:
 mov r2, #0
 strb r2,[r3]
-
-
-
-CheckDefender:
-mov r6,r4
-mov r4,r5
-mov r5,r6
-
-mov r0,r4
-add r0,#0x4A
-ldrh r0,[r0]
-mov r2,#0xFF
-and r0,r2
-cmp r0,#0
-beq CheckDfdrCritAvo
-
-
-ldr r3,=ItemAvoBonusList
-
-AvoLoopStart2:
-ldrb r2,[r3]
-cmp r2,#0
-beq CheckDfdrCritAvo
-
-cmp r2,r0
-beq NextDfdrAvo
-
-add r3,#3
-b AvoLoopStart2
-
-
-
-NextDfdrAvo:
-ldrb r0,[r3,#1]
-cmp r0, #0
-beq DfdrAvoBonus
-cmp r0, #1
-beq DfdrAvoPenalty
-b CheckDfdrCritAvo
-
-
-DfdrAvoBonus:
-ldrb r1,[r3,#2] @Value
-
-mov r3,r4		@Dfdr
-add r3, #0x62  	@Avo
-ldrb r2,[r3]	
-add r2,r1		@add given value
-strb r2,[r3]	@store
-b CheckDfdrCritAvo
-
-
-DfdrAvoPenalty:
-ldrb r1,[r3,#2]  @Value
-
-mov r3,r4		@Dfdr
-add r3,#0x62  		@given stat
-ldrb r2,[r3]	
-cmp r2, r1
-ble SetZeroAvoD
-
-sub r2,r1		@subtract given value
-strb r2,[r3]	@store
-b CheckDfdrCritAvo
-
-SetZeroAvoD:
-mov r2, #0
-strb r2,[r3]
-
-
-CheckDfdrCritAvo:
-
-mov r0,r4
-add r0,#0x4A
-ldrh r0,[r0]
-mov r2,#0xFF
-and r0,r2
-cmp r0,#0
-beq End
-
-ldr r3,=ItemDdgBonusList
-
-DdgLoopStart2:
-ldrb r2,[r3]
-cmp r2,#0
-beq End
-
-cmp r2,r0
-beq NextDfdrDdg
-
-add r3,#3
-b DdgLoopStart2
-
-
-
-NextDfdrDdg:
-ldrb r0,[r3,#1]
-cmp r0, #0
-beq DfdrDdgBonus
-cmp r0, #1
-beq DfdrDdgPenalty
-b End
-
-
-DfdrDdgBonus:
-ldrb r1,[r3,#2] @Value
-
-mov r3,r4		@Dfdr
-add r3,#0x68  		@given stat
-ldrb r2,[r3]	
-add r2,r1		@add given value
-strb r2,[r3]	@store
-b End
-
-
-DfdrDdgPenalty:
-ldrb r1,[r3,#2] @Value
-
-mov r3,r4		@Dfdr
-add r3,#0x68  		@given stat
-ldrb r2,[r3]	
-cmp r2, r1
-ble SetZeroDdgD
-
-sub r2,r1		@subtract given value
-strb r2,[r3]	@store
-b End
-
-SetZeroDdgD:
-mov r2, #0
-strb r2,[r3]
-
 
 End:
 pop {r4-r7}
